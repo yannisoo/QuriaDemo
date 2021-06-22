d5cbf46118f4be6f1a8523ef029c18af6ec11fbe
Quentin FLE <karonad@MacBook-Pro-de-Quentin.local>
2021-06-22 14:19:41 +0200

    Ajout page armor + header et footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArmorComponent } from './components/armor/armor.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'armor', component: ArmorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/global/header/header.component';
+import { FooterComponent } from './components/global/footer/footer.component';
+import { ArmorComponent } from './components/armor/armor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ArmorComponent
   ],
   imports: [
     BrowserModule,
